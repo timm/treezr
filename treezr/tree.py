2af2d5cef86c28eb87504bedd116cc60328b2204
timm <timm@ieee.org>
2025-09-10 00:14:40 -0400

    sadas

--- treezr/tree.py
+++ treezr/tree.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3 
 from types import SimpleNamespace as o
-from typing import Any,List,Iterator
+from typing import Any,Iterator
 import traceback, random, time, math, sys, re
 sys.dont_write_bytecode = True
 
@@ -11,7 +11,8 @@ Num  = list
 Sym  = dict
 Col  = Num | Sym
 
-the = o(p=2, few=64, file="../../moot/optimize/misc/auto93.csv")
+the = o(p=2, few=64, file="../../moot/optimize/misc/auto93.csv",
+        seed=1234567891)
 
 #--------------------------------------------------------------------
 def Data(src):
@@ -19,7 +20,7 @@ def Data(src):
   for n,row in enumerate(src):
     if n==0: data.cols = Cols(row)
     else:
-      [add(col,row[c]) for c,col in data.cols.all()]
+      [add(col,row[c]) for c,col in data.cols.all.items()]
       data.rows += [row]        
   for col in data.cols.all: ok(col)
   return data
@@ -27,13 +28,15 @@ def Data(src):
 def clone(data,rows=[]):
    return Data([data.cols.rows] + rows)
 
-def Cols(names : List[str]) -> o:
+def Cols(names : list[str]) -> o:
   what = lambda s: Num() if s[0].isupper() else Sym()
   all  = {c:what(s) for c,s in enumerate(names)}
-  return o(names = names, all = all, 
-    x = {c:all[n] for c,s in enumerate(names) if s[-1] not in "X-+"]},
-    y = {c:all[n] for c,s in enumerate(names) if s[-1] in "-+"]})
+  y    = {c:all[c]  for c,s in enumerate(names) if s[-1] in "-+"},
+  w    = {c:(0 if s[-1] =="-" else 1) for c,s in enumerate(names) if c in y}
+  return o(names = names, all = all, w=w, y=y,
+    x = {c:all[c] for c,s in enumerate(names) if s[-1] not in "X-+"})
 
+#--------------------------------------------------------------------
 def adds(src, col=None):
   for x in src:
     col = col or (Sym if type(x) is str else Num)()
@@ -45,38 +48,48 @@ def add(col,x):
     if type(col) is Num: col.append(x)
     else: col[x] = 1 + col.get(x,0)
 
-def ok(col,x):
+def ok(col):
   return sorted(col) if type(col) is Num else col
 
 def div(col):
   if type(col) is Num:
     n=len(col)//10; return (col[9*n] - col[n])/2.56
   else:
-    n=len(col)
-    return -sum(p*math.log(p) for m in col.values() if (p : =m/n) >0)
+    N = sum(col.values())
+    return -sum(p*math.log(p) for n in col.values() if (p:=n/N) >0)
 
 def norm(col,x):
   lo,hi = col[0],col[-1]
   return x if x=="?" else (x - lo)/(hi - lo + 1e-32)
 
-def dist(data:Data, row1:Row, row2:Row) -> float:
-  n,d = 0,0
-  for c,col in data.cols.x.items():
-    n += 1
-    d += _dist(col,row1[c], row2[c])**the.p
+#--------------------------------------------------------------------
+def dist(src) -> float:
+  d,n = 0,0
+  for v in src: n,d = n+1, d + v**the.p;
   return (d/n) ** (1/the.p)
 
-def _dist(col, a,b):
-  if a==b=="?": return 1
-  if type(col) is Sym: return a != b
-  a,b = norm(col,a), norm(col,b)
-  a = a if a != "?" else (0 if b>0.5 else 1)
-  b = b if b != "?" else (0 if a>0.5 else 1)
-  return abs(a - b)
+def disty(data:Data, row:Row) -> float:
+  return dist(abs(norm(col, row[c]) - w[c]) 
+              for c,col in data.cols.y.items())
 
+def besty(data:Data,rows=None) -> list[Row]:
+  return min(rows or data.rows, key=lambda r: disty(data,r))
+
+def distx(data:Data, row1:Row, row2:Row) -> float:
+  def _aha(col, a,b):
+    if a==b=="?": return 1
+    if type(col) is Sym: return a != b
+    a,b = norm(col,a), norm(col,b)
+    a = a if a != "?" else (0 if b>0.5 else 1)
+    b = b if b != "?" else (0 if a>0.5 else 1)
+    return abs(a - b)
+  return dist(_aha(col, row1[c], row2[c])  
+              for c,col in data.cols.x.items())
+
+#--------------------------------------------------------------------
 def projectToLine(data,rows):
   zero, *few = random.choices(rows, k=the.Few)
-  D  = lambda r1,r2:dist(data,r1,r2)
+  D  = lambda r1,r2:distx(data,r1,r2)
   lo = max(few, key= lambda r: D(zero,r))
   hi = max(few, key= lambda r: D(lo,r))
   c  = D(lo,hi)
@@ -103,8 +116,8 @@ def cut2(ids):
 def cluster(data, rows=None, stop=4, cut=cut1):
   def go(rows, cid):
     if len(rows) >= stop:
-      if n := cut(projectToLine(data,rows))
-        return go(rows[n:]], 1 + go(rows[:n], cid))
+      if n := cut(projectToLine(data,rows)):
+        return go(rows[n:], 1 + go(rows[:n], cid))
     for row in rows: ids[id(row)] = cid
     return cid
   ids = {}
@@ -163,6 +176,7 @@ def treeCuts(col:o, rows:list[Row], Y:callable, Klass:callable) -> o:
 
   return (_sym if col.it is Sym else _num)(col)
 
+#--------------------------------------------------------------------
 def coerce(s:str) -> Atom:
   for fn in [int,float]:
     try: return fn(s)
@@ -176,10 +190,45 @@ def csv(file: str ) -> Iterator[Row]:
       if (line := line.split("%")[0]):
         yield [coerce(s) for s in line.split(",")]
 
-def shuffle(lst:List) -> List:
+def shuffle(lst:list) -> list:
   random.shuffle(lst); return lst
 
 def value(x):
   return -1e32 if x=="?" else x
 
+def dataDwin(file=None):
+  data = Data(csv(file or the.file))
+  D    = lambda row: disty(data,row)
+  b4   = adds(D(row) for row in data.rows)
+  return data, D, lambda v: 100*(1 - (v - b4.lo)/(b4.mu - b4.lo))
+  
+def eg__demo():
+  random.seed(the.seed)
+  data,D,win = dataDwin(the.file)
+  for b in range(10,200,20):
+    print(".")
+    alls=[]
+    somes=[]
+    for _ in range(50):
+      somes += [int(win(D(min(random.choices(data.rows,k=b//2),key=D))))]
+      rows = random.choices(data.rows,k=b)
+      ids  = distClusters(data,rows)
+      tree = Tree(clone(data, rows),
+                Y=lambda row: ids[id(row)],
+                Klass=Sym)
+      mid=lambda a: a[len(a)//2]
+      alls += [max([int(win(D(mid(x.rows)))) for n,x in treeNodes(tree)])]
+    print(b//2, mid(sorted(somes)), mid(sorted(alls)))
 
+#---------------------------------------------------------------------
+def main(settings : o, funs: dict[str,callable]) -> o:
+  for n,s in enumerate(sys.argv):
+    if (fn := funs.get(f"eg{s.replace('-', '_')}")):
+      random.seed(settings.seed); fn()
+    else:
+      for key in vars(settings):
+        if s=="-"+key[0]: 
+          settings.__dict__[key] = coerce(sys.argv[n+1])
+
+if __name__ == "__main__": 
+  main(the, globals())
