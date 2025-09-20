874bc756bc433e84157a31671e9606d038de18a7
timm <timm@ieee.org>
2025-09-20 09:47:35 -0400

    asdas

--- treezr/tree.py
+++ treezr/tree.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3 
 from types import SimpleNamespace as o
-from typing import Iterator, Iterable
+from typing import Any, Iterator, Iterable
 import traceback, random, time, sys, re
 from math import log
 
@@ -20,75 +20,106 @@ the = o(p=2, few=64, seed=1234567891, Few=256, leaf=4,
 
 big = 1e32
 
-def ok(col): 
-  return sorted(col) if type(col) is Num else col
+#--------------------------------------------------------------------
+def Num(at=0,s=" "): 
+  "Create a numeric column summarizer"
+  return o(it=Num, at=at, txt=s, n=0, mu=0, m2=0, sd=0, 
+           hi=-big, lo=big, more = 0 if s[-1] == "-" else 1)
 
-def div(col): 
-  return stdev(col) if type(col) is Num else entropy(col)
+def Sym(at=0,s=" "): 
+  "Create a symbolic column summarizer"
+  return o(it=Sym, at=at, txt=s, n=0, has={})
 
-def mid(col): 
-  return col[len(col)//2] if type(col) is Num else max(col,key=col.get)
+def Cols(names : list[str]) -> o:
+  "Create column summaries from column names"
+  all=[(Num if s[0].isupper() else Sym)(c,s) 
+        for c,s in enumerate(names)]
+  return o(it=Cols, names = names, all = all, 
+           x = [col for col in all if col.txt[-1] not in "X-+"],
+           y = [col for col in all if col.txt[-1] in "-+"])
 
-def stdev(a):
-  n = len(a)//10; return (a[n*9] - a[n]) / 2.56
+def Data(src) -> o:
+  "Create data structure from source rows"
+  src = iter(src)
+  return adds(src, o(it=Data, n=0, mid=None, rows=[], kids=[], 
+                     ys=None, cols=Cols(next(src)))) 
 
-def entropy(d):
-  N = sum(d.values())
-  return -sum(p*log(p,2) for n in d.values() if (p:=n/N) if n>0)
+def clone(data:Data, rows=None) -> o:
+  "Create new Data with same columns but different rows"
+  return adds(rows or [], Data([data.cols.names]))
 
 #--------------------------------------------------------------------
-def Data(src: Iterable):
-  rows = iter(src)
-  data = o(cols=Cols(next(rows)), rows=[])
-  [adds(data,row) for row in rows]
-  data.all = [ok(col) for col in data.col.all]
-  return data
+def adds(src, it=None) -> o:
+  "Add multiple items to a summarizer"
+  it = it or Num()
+  [add(it,x) for x in src]
+  return it
 
-def adds(data,row):
-  data.rows += [row]
-  for c in data.cols.all: add(data.cols.all, c, row[c]) 
-  return row
+def sub(x:o, v:Any, zap=False) -> Any: 
+  "Remove value from summarizer"
+  return add(x,v,-1,zap)
 
-def add(cols,c,v):
-  if v != "?":
-    if type(cols[c]) is Sym: 
-      cols[c][v] = 1 + cols[c].get(v,0)
-    else: 
-      cols[c] = (min(v,cols[c][0]), max(v,cols[c][1]))
+def add(x: o, v:Any, inc=1, zap=False) -> Any:
+  "incrementally update Syms,Nums or Datas"
+  if v == "?": return v
+  x.n += inc
+  if   x.it is Sym: x.has[v] = inc + x.has.get(v,0)
+  elif x.it is Num:
+    x.lo, x.hi = min(v, x.lo), max(v, x.hi)
+    if inc < 0 and x.n < 2:
+      x.sd = x.m2 = x.mu = x.n = 0
+    else:
+      d     = v - x.mu
+      x.mu += inc * (d / x.n)
+      x.m2 += inc * (d * (v - x.mu))
+      x.sd  = 0 if x.n < 2 else (max(0,x.m2)/(x.n-1))**.5
+  elif x.it is Data:
+    x.mid = None
+    if inc > 0: x.rows += [v]
+    elif zap: x.rows.remove(v) # slow for long rows
+    [add(col, v[col.at], inc) for col in x.cols.all]
+  else: raise TypeError(f"cannot add to {type(x)}")
   return v
 
-def clone(data,rows=[]):
-  return Data([data.cols.names] + rows)
+#--------------------------------------------------------------------
+def norm(num:Num, v:float) -> float:  
+  "Normalize a value to 0..1 range"
+  return v if v=="?" else (v - num.lo) / (num.hi - num.lo + 1E-32)
 
-def Cols(txt : list[str]) -> o:
-  tmp = {c for c,s in enumerate(txt) if s[-1] != "X"}
-  all = {c:Num() if txt[c][0].isupper() else Sym() for c in tmp} 
-  y   = {c:txt[c][-1]!="-" for c in tmp if txt[c][-1] in "-+"}
-  x   = {c for c in tmp if c not in y}
-  return o(names=txt, all=all, y=y, x=x)
+def mid(col: o) -> Atom:
+  "Get central tendency of one column"
+  return max(col.has, key=col.has.get) if col.it is Sym else col.mu
+
+def div(col:o) -> float:
+  "Return the central tendnacy for one column."
+  if col.it is Num: return col.sd
+  vs = col.has.values()
+  N  = sum(vs)
+  return -sum(p*log(p,2) for n in vs if (p:=n/N) > 0)
 
 #--------------------------------------------------------------------
-def norm(x,lo,hi): return (x - lo) / (hi - lo + 1e-32) 
-
-def dist(src):
-  n,d = 0,0
-  for x in src: n,d = n + 1, d + x ** the.p
+def dist(src) -> float:
+  "Calculate Minkowski distance"
+  d,n = 0,0
+  for v in src: n,d = n+1, d + v**the.p;
   return (d/n) ** (1/the.p)
 
 def disty(data:Data, row:Row) -> float:
-  return dist(abs(norm(row[c], *data.cols.all[c]) - w)
-              for c,w in data.cols.y.items())
+  "Distance from row to best y-values"
+  return dist(abs(norm(c, row[c.at]) - c.more) for c in data.cols.y)
 
 def distx(data:Data, row1:Row, row2:Row) -> float:
-  def _aha(col, a,b):
-    if a==b=="?": return 1
-    if type(col) is Sym: return a != b
-    a,b = norm(a, *col), norm(b, *col)
-    a = a if a != "?" else (0 if b>0.5 else 1)
-    b = b if b != "?" else (0 if a>0.5 else 1)
-    return abs(a - b)
-  return dist(_aha(data.cols.all[x], row1[x], row2[x])  
-              for x in data.cols.x)
+  "Distance between two rows using x-values"
+  return dist(_aha(c, row1[c.at], row2[c.at])  for c in data.cols.x)
+
+def _aha(col, a,b):
+  "David Aha's distance function."
+  if a==b=="?": return 1
+  if col.it is Sym: return a != b
+  a,b = norm(col,a), norm(col,b)
+  a = a if a != "?" else (0 if b>0.5 else 1)
+  b = b if b != "?" else (0 if a>0.5 else 1)
+  return abs(a - b)
 
 #--------------------------------------------------------------------
 def project(data,rows):
@@ -114,7 +145,6 @@ def cluster(data, rows=None, stop=None):
   return ids
 
 #-------------------------------------------------------------
-# klass 
 def treeSelects(row:Row, op:str, at:int, y:Atom) -> bool: 
   "Have we selected this row?"
   if (x:=row[at]) == "?" : return True
@@ -122,46 +152,44 @@ def treeSelects(row:Row, op:str, at:int, y:Atom) -> bool:
   if op == "=="          : return x == y
   if op == ">"           : return x > y
 
-def Tree(data, rows=None, Y=None,  how=None):
+def Tree(data, rows=None, Y=None, Klass=Num, how=None):
   "Create tree from list of lists"
   rows = rows or data.rows
   Y    = Y or (lambda row: disty(data,row))
-  tree = o(rows=rows, how=how, kids=[], mu=mean([Y(r) for r in rows]))
+  tree = o(rows=rows, how=how, kids=[], 
+           mu=mid(adds(Y(r) for r in rows)))
   if len(rows) >= the.leaf:
-    spread, cuts = min(treeCuts(x, data.cols.all[x], rows,Y) 
-                       for x in data.cols.x)
+    spread, cuts = min(treeCuts(c,rows,Y,Klass) for c in data.cols.x)
     if spread < big:
       for cut in cuts:
         subset = [r for r in rows if treeSelects(r, *cut)]
         if the.leaf <= len(subset) < len(rows):
-          tree.kids += [Tree(data, subset, Y, cut)]
+          tree.kids += [Tree(data, subset, Y, Klass, cut)]
   return tree
 
-def treeCuts(at, col, rows, Y:callable):
+def treeCuts(col, rows, Y:callable, Klass:callable):
   "Return best cut for column at position 'at'"
-  xys = sorted([(r[at], Y(r)) for r in rows if r[at] != "?"])
-  return (_symCuts if type(col) is Sym else _numCuts)(at,xys)
+  xys = sorted([(r[col.at], Y(r)) for r in rows if r[col.at] != "?"])
+  return (_symCuts if col.it is Sym else _numCuts)(col.at,xys,Y,Klass)
 
-def _symCuts(at,xys) -> (float, list):
+def _symCuts(at,xys,Y,Klass) -> (float, list):
   "Cuts for symbolic column."
   d = {}
-  for x,y in xys:
-    d[x]    = d.get(x) or {}
-    d[x][y] = 1 + d[x].get(y,0) 
-  here = sum(sum(ys.values())/len(xys) * ent(ys) for ys in d.values())
+  for x, y in xys:
+    d[x] = d.get(x) or Klass()
+    add(d[x], y)
+  here = sum(ys.n/len(xys) * div(ys) for ys in d.values())
   return here, [("==", at, x) for x in d]
 
-def _numCuts(at,xys) -> (float, list):
+def _numCuts(at,xys,Y,Klass) -> (float, list):
   "Cuts for numeric columns."
-  spread, cuts, left, right = big, [], {}, {}
-  for _, y in xys: 
-    right[y] = 1 + right.get(y,0)
+  spread, cuts, left, right = big, [], Klass(), Klass()
+  [add(right,y) for _, y in xys]
   for i, (x, y) in enumerate(xys[:-1]):
-    right[y] -= 1
-    left[y]   = 1 + left.get(y,0)
+    add(left, sub(right, y))
     if x != xys[i+1][0]:
       if the.leaf <= i < len(xys) - the.leaf:
-        now = (i*ent(left) + (len(xys) - i)*ent(right)) / len(xys)
+        now = (left.n*div(left) + right.n*div(right)) / (left.n+right.n)
         if now < spread:
           spread = now
           cuts = [("<=", at, x), (">", at, x)]
@@ -198,8 +226,6 @@ def treeShow(data,tree,win=None):
                            key=lambda k: -n[k]))
 
 #--------------------------------------------------------------------
-def mean(lst): return sum(lst) / len(lst)
-
 def coerce(s:str) -> Atom:
   for fn in [int,float]:
     try: return fn(s)
@@ -232,7 +258,6 @@ def eg__tree():
   data  = Data(csv(the.file))
   data1 = clone(data, random.choices(data.rows, k=32))
   tree  = Tree(data1)
-  print(len([leaf for _,leaf in treeNodes(tree) if not leaf.kids]))
   treeShow(data1, tree)
 
 def eg__demo():
